00d7ea15e20f7205c7fda9fc142e25ecdf958f32
marielle-lopez <mariellelouisse@gmail.com>
2025-03-01 20:59:40 +1100

    Create `shuffle()` function intended for 'shuffling' a list of cards

---
+++ src/helpers/shuffle.tsx
@@ -0,0 +1,12 @@
+import CardModel from '../models/card'
+
+const shuffle = (cards: CardModel[]) : CardModel[] => {
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+
+    return cards;
+}
+
+export default shuffle;
