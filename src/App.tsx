48051270aa1eb912a2e55110150a7b2746250838
marielle-lopez <mariellelouisse@gmail.com>
2025-03-01 20:03:23 +1100

    Display each card from the deck

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import deck from './data/cards.json'
 import './App.css'
+import { useState, useEffect } from 'react';
+
+interface Card {
+  suit: string;
+  value: string;
+  points: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState<Card[]>([]);
+
+  useEffect(() => {
+    const allCards: Card[] = [];
+
+    deck.forEach((suit) => {
+      suit.values.forEach((card) => {
+        let newCard = {
+          suit: suit.suit,
+          value: card.value,
+          points: card.points
+        }
+
+        allCards.push(newCard);
+      })
+    })
+
+    setCards(allCards);
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Goldfish</h1>
+      {
+        cards && cards.map((card) => <p>{card.suit}{card.value}</p>)
+      }
     </>
   )
 }
